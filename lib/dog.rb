69306edc26da2a47cd2efc0faac4af2e6ccc4b5d
James Mobus <mobusj@live.com>
2019-11-08 01:11:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,5 +79,10 @@ class Dog
   end
   
   def self.find_or_create_by(name, breed)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?, breed = ?
+      SQL
+      
+    DB[:conn].execute(sql, name, breed)
   end
 end
